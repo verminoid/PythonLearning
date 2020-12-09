1938bfa839c19cca4882bc7b9d2aa8bf928dd9c2
verminoid <verminoid@yandex.ru>
2020-12-09 21:53:14 +0300

    master

--- Metrics/metrics.py
+++ Metrics/metrics.py
@@ -1,23 +1,21 @@
 import socket
 import time
+from collections import defaultdict
 
 class ClientError(Exception):
     pass
 
 def ParcerGet(data):
-    parced_dict = dict()
-    for line in data.split("\n"):
-        if line == "ok":
-            continue
-        elif line is None:
-            break
-        str = line.split(" ")
-        temp = parced_dict.get(str[0])
-        if temp in None:
-            parced_dict.update(str[0],tuple(int(str[2]),float(str[1])))
-        else:
-            parced_dict[str[0]] = temp.append(tuple(int(str[2]),float(str[1])))
-    for item in parced_dict:
+    if data == "ok\n\n":
+        return dict()
+    if data == 'error\nwrong command\n\n':
+        raise ClientError()
+    items = data.lstrip('ok\n').rstrip('\n\n')
+    items = [x.split() for x in items.split('\n')]
+    parced_dict = defaultdict(list)
+    for key, metric, timestamp in items:
+        parced_dict[key].append((int(timestamp), float(metric)))
+    for item in parced_dict.values():
         item.sort()
     return parced_dict
 
@@ -32,30 +30,28 @@ class Client:
         self._host = host
         self._port = port
         self._timeout = timeout
-        # TODO создать соединение здесь
-                # создать удаление с закрытием сокета
+        self.sock = socket.create_connection((self._host, self._port),self._timeout)
+
+    def __del__(self):
+        self.sock.close()
 
     def put(self, key, value, timestamp=None):
-        with socket.create_connection((self._host, self._port),self._timeout) as sock:
-            try:    
-                if timestamp is None:
-                    timestamp = int(time.time())
-                sock.sendall(("put {} {} {}\n".format(key, value, timestamp)).encode("utf8"))
-                ansver = sock.recv(1024).decode("utf8")
-                if "ok\n\n" not in ansver:
-                    raise ClientError
-            except socket.error:
-                raise ClientError
+        try:    
+            if timestamp is None:
+                timestamp = int(time.time())
+            self.sock.sendall(("put {} {} {}\n".format(key, value, timestamp)).encode("utf8"))
+            ansver = self.sock.recv(1024).decode("utf8")
+            if "ok\n\n" not in ansver:
+                raise ClientError()
+        except socket.error:
+            raise ClientError()
             
 
 
     def get(self, key):
         
-        with socket.create_connection((self._host, self._port),self._timeout) as sock:
-            sock.sendall("get {}\n".format(key).encode("utf8"))
-            ansver = sock.recv(1024).decode("utf8")
-            if "ok\n" in ansver:
-                return ParcerGet(ansver)
-            else:
-                raise ClientError
+        self.sock.sendall("get {}\n".format(key).encode("utf8"))
+        ansver = self.sock.recv(1024).decode("utf8")
+        return ParcerGet(ansver)
+        
         
