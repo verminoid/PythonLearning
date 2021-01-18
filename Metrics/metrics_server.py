6a8b650df125e684b4124d962270fa7a77beed85
verminoid <verminoid@yandex.ru>
2021-01-18 13:02:14 +0300

    master

--- Metrics/metrics_server.py
+++ Metrics/metrics_server.py
@@ -1,14 +1,11 @@
 import asyncio
 from collections import defaultdict
 
-def process_data(data):
-    if "put" in data:
-        quere = data.lstrip('put ').rstrip('\n')
-        quere = quere.split()
-        dict[quere[0]].append(float(quere[1]),int(quere[2]))
-    return 
 
 class ClientServerProtocol(asyncio.Protocol):
+
+    dict = defaultdict(list)
+
     def connection_made(self, transport):
         self.transport = transport
 
@@ -16,9 +13,9 @@ class ClientServerProtocol(asyncio.Protocol):
         resp = process_data(data.decode())
         self.transport.write(resp.encode())
 
+
 def run_server(host,port):
     
-    dict = defaultdict(list)
     loop = asyncio.get_event_loop()
     coro = loop.create_server(
         ClientServerProtocol,
@@ -35,3 +32,32 @@ def run_server(host,port):
     server.close()
     loop.run_until_complete(server.wait_closed())
     loop.close()
+
+def process_data(data):
+    quere = data.split()
+    com = quere[0]
+    out = ""
+    if com not in ("put","get"):
+        out = "error\nwrong command\n\n"
+    else: 
+        out = "ok\n"
+    if com == "put":
+        dataset = quere[1:]
+        key, metric, timestamp = dataset
+        ClientServerProtocol.dict[key].append((float(metric),int(timestamp)))
+       
+    if com == "get":
+        if len(quere)>1:
+            k = quere[1]
+            if k == "*":
+                for key, values in ClientServerProtocol.dict.items():
+                    for metric, timestamp in values:
+                        out += "{} {} {}\n".format(key, metric, timestamp)
+            else:
+                for (metric, timestamp) in ClientServerProtocol.dict[k]:
+                    out += "{} {} {}\n".format(k, metric, timestamp)
+    out += "\n"
+    
+    return out
+
+run_server("localhost", 8888)
