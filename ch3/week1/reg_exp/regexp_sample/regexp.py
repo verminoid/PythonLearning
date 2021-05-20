a3d1d4171756e297626fee80bbf42fba5de51563
verminoid <verminoid@yandex.ru>
2021-05-20 18:11:15 +0300

    regexp

---
+++ ch3/week1/reg_exp/regexp_sample/regexp.py
@@ -0,0 +1,12 @@
+def calculate(data, findall):
+    matches = findall(r"([a-c])([\+\-]?)\=([a-c]?)([\+\-]?\d*)")  # Если придумать хорошую регулярку, будет просто
+    for v1, s, v2, n in matches:  # Если кортеж такой структуры: var1, [sign]=, [var2], [[+-]number]
+        # Если бы могло быть только =, вообще одной строкой все считалось бы, вот так:
+        if s == "":
+            data[v1] = data.get(v2, 0) + int(n or 0)
+        if s == "+":
+            data[v1] = data.get(v1, 0) + data.get(v2, 0) + int(n or 0)
+        if s == "-":
+            data[v1] = data.get(v1, 0) - (data.get(v2, 0) + int(n or 0))
+
+    return data
