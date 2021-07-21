90d573211edf412df5326f4b9c2bb10fd751b2d0
verminoid <verminoid@yandex.ru>
2021-07-21 15:45:33 +0300

    django router

---
+++ ch3/week4/coursera_assignment_tmp-master/routing/views.py
@@ -0,0 +1,72 @@
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
+
+def simple_route(request):
+    response = HttpResponse()
+    if request.method != 'GET':
+        response.status_code = 405
+    else:
+        response.status_code = 200
+        
+
+    return response
+
+def slug_route(request):
+    response = HttpResponse()
+    body = request.path.split("/")[3]
+    response.status_code = 200
+    response.write(body)    
+
+    return response
+
+def sum_route(request):
+    response = HttpResponse()
+    body = request.path.split("/")
+    sum = int(body[3])+int(body[4])
+    response.status_code = 200
+    response.write(sum)    
+
+    return response
+
+def isint(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+@require_http_methods(["GET"])
+def sum_get_method(request):
+    response = HttpResponse()
+    try:
+        a = request.GET["a"]
+        b = request.GET["b"]
+        if (isint(a) and isint(b)):
+            sum = int(a)+int(b)
+            response.status_code = 200
+            response.write(sum)
+        else:
+            response.status_code = 400
+    except KeyError:
+        response.status_code = 400
+    
+
+    return response
+
+@require_http_methods(["POST"])
+def sum_post_method(request):
+    response = HttpResponse()
+    try:
+        a = request.POST["a"]
+        b = request.POST["b"]
+        if (isint(a) and isint(b)):
+            sum = int(a)+int(b)
+            response.status_code = 200
+            response.write(sum)
+        else:
+            response.status_code = 400
+    except KeyError:
+        response.status_code = 400
+    
+
+    return response
